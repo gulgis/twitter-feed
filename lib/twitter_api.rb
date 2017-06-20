28a000074a8d42f880b414d41bd06da84eb6da29
Willian Gulgielmin <gulgielmin.w@gmail.com>
2017-06-20 03:26:16 -0300

    Confirguracao da twitter API

---
+++ lib/twitter_api.rb
@@ -0,0 +1,15 @@
+class TwitterApi
+  def self.our_public_tweets
+    client.user_timeline('netflix', count: 5, exclude_relplies: true, include_rts: false)
+  end
+
+
+  def self.client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      #config.access_token        = "YOUR_ACCESS_TOKEN"
+      #config.access_token_secret = "YOUR_ACCESS_SECRET"
+    end
+  end
+end
