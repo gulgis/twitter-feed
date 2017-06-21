d13569e9d63ce7edd0578c858f7a71d2d508adbf
Willian Gulgielmin <gulgielmin.w@gmail.com>
2017-06-20 21:59:49 -0300

    Adicionar usuario e mostrar cinco tweets para cada um

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,6 +1,6 @@
 class TwitterApi
-  def self.our_public_tweets
-    client.user_timeline('netflix', count: 5, exclude_relplies: true, include_rts: false)
+  def self.get_five_tweets(username)
+    client.user_timeline(username, count: 5, exclude_relplies: true, include_rts: false)
   end
 
 
@@ -8,8 +8,8 @@ class TwitterApi
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      #config.access_token        = "YOUR_ACCESS_TOKEN"
-      #config.access_token_secret = "YOUR_ACCESS_SECRET"
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
   end
 end
